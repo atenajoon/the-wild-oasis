9bf7a0ffc159354687144d633fc9ba78690b4538
Atena Razm <atenarazm@gmail.com>
2024-09-05 11:02:43 -0700

    globalStyles with Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
+
+const StyledApp = styled.main`
+  background-color: red;
+`;
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Input></Input>
+        <h1>Hi</h1>
+        <Button>Check in</Button>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
