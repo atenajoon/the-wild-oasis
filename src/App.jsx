69db43a8eebc724eef3091a82afce1f2fdc83cac
Atena Razm <atenarazm@gmail.com>
2024-09-05 11:35:47 -0700

    as props & css fnc to create Heading, Input, and Button styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/Input';
+import Heading from './ui/Heading';
 
 const StyledApp = styled.main`
   background-color: red;
@@ -11,9 +12,14 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Input></Input>
-        <h1>Hi</h1>
+        <Heading as='h1'>The Wild Oasis</Heading>
+        <Heading as='h2'>Check in and out</Heading>
+
         <Button>Check in</Button>
+        <Button>Check out</Button>
+        <Heading as='h3'>Form</Heading>
+        <Input type='number' placeholder='Number of guests' />
+        <Input />
       </StyledApp>
     </>
   );
