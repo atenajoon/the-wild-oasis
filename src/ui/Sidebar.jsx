c5f8d1b75979af6d4a7c79684cac18e719f9ba7b
Atena Razm <atenarazm@gmail.com>
2024-09-09 10:03:47 -0700

    header, main, sidebar styled with styled-components

---
+++ src/ui/Sidebar.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledSidebar = styled.aside`
+  background-color: blue;
+  padding: 3.2rem 2.4rem;
+  border-right: 1px solid var(--color-grey-100);
+`;
+function Sidebar() {
+  return <StyledSidebar>Side</StyledSidebar>;
+}
+
+export default Sidebar;
