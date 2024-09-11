d7f43bbf8ad49b63d0293384338280f9e5aac21f
Atena Razm <atenarazm@gmail.com>
2024-09-11 11:15:46 -0700

    uploading new cabin with image to supabase tables

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://rvclruvjbbwbyhttihvb.supabase.co';
+export const supabaseUrl = 'https://rvclruvjbbwbyhttihvb.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2Y2xydXZqYmJ3YnlodHRpaHZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5MDY0NzAsImV4cCI6MjA0MTQ4MjQ3MH0.0GmI-TewIMK9mkiEng2jNGdLbrWulHOyWcuL3uM0XeQ';
 const supabase = createClient(supabaseUrl, supabaseKey);
