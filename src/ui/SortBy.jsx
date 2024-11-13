0a9a123cf1763428c8e6612657d6db199bf13496
Atena Razm <atenarazm@gmail.com>
2024-11-13 11:54:42 -0800

    sortBy logic written

--- src/ui/SortBy.jsx
+++ src/ui/SortBy.jsx
@@ -1,5 +1,23 @@
+import { useSearchParams } from 'react-router-dom';
+import Select from './Select';
+
 function SortBy({ options }) {
-  return <div>sort by</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const sortBy = searchParams.get('sortBy') || '';
+
+  function handleChange(e) {
+    searchParams.set('sortBy', e.target.value);
+    setSearchParams(searchParams);
+  }
+
+  return (
+    <Select
+      options={options}
+      value={sortBy}
+      type='white'
+      onChange={handleChange}
+    />
+  );
 }
 
 export default SortBy;
