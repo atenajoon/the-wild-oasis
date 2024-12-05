83d8a16222c12eda80fea322507e9624d9034c97
Atena Razm <atenarazm@gmail.com>
2024-12-05 10:59:44 -0800

    prefetching the next page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,10 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
 import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   // FILTER
   const filterValue = searchParams.get('status');
@@ -21,6 +22,7 @@ export function useBookings() {
   // PAGINATION
   const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
+  // Query
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -30,5 +32,10 @@ export function useBookings() {
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
 
+  // PRE-FETCHING
+  queryClient.prefetchQuery({
+    queryKey: ['bookings', filter, sortBy, page + 1],
+    queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+  });
   return { isLoading, error, bookings, count };
 }
