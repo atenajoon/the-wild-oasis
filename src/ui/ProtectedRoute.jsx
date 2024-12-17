77aa5117ec69be66bd6bc364e490322347d0eed8
Atena Razm <atenarazm@gmail.com>
2024-12-17 13:23:29 -0800

    only Authenticated user is loged in and navigated to Dashboard

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,12 +1,38 @@
+import styled from 'styled-components';
+import { useUser } from '../features/authentication/useUser';
+import Spinner from './Spinner';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-gray-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
   // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
 
-  // 2. While loading, show the spinner
+  // 2. If user IS NOT authenticated, redirect to the /login
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) navigate('/login');
+  }, [isAuthenticated, isLoading, navigate]);
 
-  // 3. If user IS NOT authenticated, redirect to the /login
+  // 3. While loading, show the spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
 
   // 4. If user IS authenticated render the app
-  return children;
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
