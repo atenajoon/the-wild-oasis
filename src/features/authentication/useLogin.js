f14f98b93cea30befe41f73bf34f751604a8c6d5
Atena Razm <atenarazm@gmail.com>
2024-12-17 13:36:55 -0800

    manually set user data in React Query cache

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,9 +1,10 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { login as loginApi } from '../../services/apiAuth';
 import toast from 'react-hot-toast';
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
@@ -13,7 +14,7 @@ export function useLogin() {
         password,
       }),
     onSuccess: (user) => {
-      console.log(user);
+      queryClient.setQueriesData(['user'], user);
       navigate('/dashboard');
     },
     onError: (err) => {
