e8d9dbb55675a51067585c944c08abff4075787b
Atena Razm <atenarazm@gmail.com>
2025-01-07 23:05:03 -0800

    darkMode context used in Headers and logo

--- src/ui/DarkModeToggle.jsx
+++ src/ui/DarkModeToggle.jsx
@@ -1,10 +1,13 @@
-import { HiOutlineMoon } from 'react-icons/hi2';
+import { HiOutlineMoon, HiOutlineSun } from 'react-icons/hi2';
+import { useDarkMode } from '../context/DarkModeContext';
 import ButtonIcon from './ButtonIcon';
 
 function DarkModeToggle() {
+  const { isDarkMode, toggleDarkMode } = useDarkMode();
+
   return (
-    <ButtonIcon>
-      <HiOutlineMoon />
+    <ButtonIcon onClick={toggleDarkMode}>
+      {isDarkMode ? <HiOutlineSun /> : <HiOutlineMoon />}
     </ButtonIcon>
   );
 }
